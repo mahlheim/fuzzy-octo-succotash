caca5229fcb72d5ddd59eb0985a1b9163db19c3b
Austin Shook <austinleeshook@gmail.com>
2023-06-14 13:28:40 -0400

    Added random generation and supporting code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,67 @@
 // YouTube API key
 var YOUTUBE_API_KEY = 'AIzaSyD5N0cU-1PafmBb_Oob3mAOOecX9I-MAHs';
+// Add your YouTube Data API key here
+const apiKey = 'AIzaSyD5N0cU-1PafmBb_Oob3mAOOecX9I-MAHs';
 
-// YouTube API Player
-let player;
+// Function to generate a random song based on genre
+async function generateRandomSong(genre) {
+  try {
+    // Search for videos based on genre using YouTube API
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${genre}%20music&type=video&key=${apiKey}`, {
+      credentials: 'same-origin' // Include SameSite attribute for the cookie
+    });
+    const data = await response.json();
 
-// Called when the YouTube API script is loaded
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
+    // Get a random video from the search results
+    const randomIndex = Math.floor(Math.random() * data.items.length);
+    const randomVideo = data.items[randomIndex];
+
+    // Return the video ID and title
+    return {
+      videoId: randomVideo.id.videoId,
+      title: randomVideo.snippet.title
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+// Function to load the YouTube player
+function loadPlayer(videoId) {
+  // Create the YouTube player
+  new YT.Player('player', {
     height: '360',
     width: '640',
-    playerVars: {
-      'autoplay': 1,
-      'controls': 0,
-      'disablekb': 1,
-      'fs': 0,
-      'iv_load_policy': 3,
-      'loop': 1,
-      'modestbranding': 1,
-      'rel': 0,
-      'showinfo': 0
+    videoId: videoId,
+    events: {
+      'onReady': onPlayerReady
     }
   });
 }
 
-// Add an event listener to the dropdown items
-const dropdownItems = document.querySelectorAll('.dropdown-item[data-genre]');
+// Function to handle when the YouTube player is ready
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// Event listener for the dropdown items
+const dropdownItems = document.querySelectorAll('.dropdown-item');
 dropdownItems.forEach(item => {
-  item.addEventListener('click', () => {
-    const genre = item.getAttribute('data-genre');
-    
-    // Load the playlist based on the selected genre
-    loadPlaylist(genre);
+  item.addEventListener('click', async () => {
+    // Remove the 'active' class from all items
+    dropdownItems.forEach(item => item.classList.remove('active'));
+
+    // Add the 'active' class to the clicked item
+    item.classList.add('active');
   });
 });
 
-// Function to load a YouTube playlist based on the genre
-function loadPlaylist(genre) {
-  const playlistIds = {
-    'pop': 'PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj',
-    'rock': 'PLZN_exA7d4RVmCQrG5VlWIjMOkMFZVVOc',
-    'hip-hop': 'PL3-sRm8xAzY-556lOpSGH6wVzyofoGpzU',
-    'country': 'PL3oW2tjiIxvQW6c-4Iry8Bpp3QId40S5S'
-  };
-  
-  const playlistId = playlistIds[genre];
-  
-  if (playlistId) {
-    player.loadPlaylist({
-      list: playlistId,
-      listType: 'playlist',
-      index: 0,
-      startSeconds: 0,
-      suggestedQuality: 'default'
-    });
-  } else {
-    console.error('Invalid genre or playlist ID not found');
-  }
-}
+// Event listener for the "Go" button
+const goButton = document.querySelector('.btn-primary');
+goButton.addEventListener('click', async () => {
+  const selectedGenre = document.querySelector('.dropdown-item.active');
+  const genre = selectedGenre.dataset.genre;
+  const song = await generateRandomSong(genre);
+  loadPlayer(song.videoId);
+});
