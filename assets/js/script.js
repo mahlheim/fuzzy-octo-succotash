e95354b6d40b5fcfe4da680812f44f4f9d5426e2
Austin Shook <austinleeshook@gmail.com>
2023-06-14 12:00:42 -0400

    Updated classes in HTML and re-wrote JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,59 @@
-// YouTube and Spotify API keys
+// YouTube API key
 var YOUTUBE_API_KEY = 'AIzaSyD5N0cU-1PafmBb_Oob3mAOOecX9I-MAHs';
 
-var genreSelect = document.getElementById('genre-select');
-var playerContainer = document.getElementById('player-container');
+// YouTube API Player
+let player;
 
-genreSelect.addEventListener('change', function() {
-  var selectedGenre = genreSelect.value;
-
-  // Clear previous players
-  playerContainer.innerHTML = '';
-
-  if (selectedGenre === 'rock') {
-    // Use YouTube API
-    var youtubePlaylistId = 'YOUR_YOUTUBE_PLAYLIST_ID';
-    var youtubeApiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=${youtubePlaylistId}&key=${YOUTUBE_API_KEY}`;
-
-    // Create YouTube playlist container
-    var youtubeContainer = document.createElement('div');
-    youtubeContainer.className = 'playlist-container';
-
-    // Create YouTube Embed
-    var youtubePlayer = document.createElement('iframe');
-    youtubePlayer.width = '560';
-    youtubePlayer.height = '315';
-    youtubePlayer.allow = 'autoplay; encrypted-media';
-    youtubePlayer.allowFullscreen = true;
-
-    fetch(youtubeApiUrl)
-      .then(response => response.json())
-      .then(data => {
-        var videos = data.items;
-        var firstVideoId = videos[0].snippet.resourceId.videoId;
-        var youtubeEmbedUrl = `https://www.youtube.com/embed/${firstVideoId}`;
-
-        youtubePlayer.src = youtubeEmbedUrl;
-        youtubeContainer.appendChild(youtubePlayer);
-      })
-      .catch(error => {
-        console.error('Error loading YouTube playlist:', error);
-      });
-
-    playerContainer.appendChild(youtubeContainer);
+// Called when the YouTube API script is loaded
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    playerVars: {
+      'autoplay': 1,
+      'controls': 0,
+      'disablekb': 1,
+      'fs': 0,
+      'iv_load_policy': 3,
+      'loop': 1,
+      'modestbranding': 1,
+      'rel': 0,
+      'showinfo': 0
     }
+  });
+}
+
+// Add an event listener to the dropdown items
+const dropdownItems = document.querySelectorAll('.dropdown-item[data-genre]');
+dropdownItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const genre = item.getAttribute('data-genre');
+    
+    // Load the playlist based on the selected genre
+    loadPlaylist(genre);
+  });
+});
+
+// Function to load a YouTube playlist based on the genre
+function loadPlaylist(genre) {
+  const playlistIds = {
+    'pop': 'PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj',
+    'rock': 'PLZN_exA7d4RVmCQrG5VlWIjMOkMFZVVOc',
+    'hip-hop': 'PL3-sRm8xAzY-556lOpSGH6wVzyofoGpzU',
+    'country': 'PL3oW2tjiIxvQW6c-4Iry8Bpp3QId40S5S'
+  };
+  
+  const playlistId = playlistIds[genre];
+  
+  if (playlistId) {
+    player.loadPlaylist({
+      list: playlistId,
+      listType: 'playlist',
+      index: 0,
+      startSeconds: 0,
+      suggestedQuality: 'default'
+    });
+  } else {
+    console.error('Invalid genre or playlist ID not found');
   }
-);
+}
