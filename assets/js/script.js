c9cb114205a738cbe4f3e2f77bc06aede325ab48
jbarz <jbarz0415@gmail.com>
2023-06-14 11:04:17 -0400

    deleted spotify api js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // YouTube and Spotify API keys
 var YOUTUBE_API_KEY = 'AIzaSyD5N0cU-1PafmBb_Oob3mAOOecX9I-MAHs';
-var SPOTIFY_API_KEY = 'b73dead14b8c4d77a34b8fe38c1f9213';
 
 var genreSelect = document.getElementById('genre-select');
 var playerContainer = document.getElementById('player-container');
@@ -42,47 +41,6 @@ genreSelect.addEventListener('change', function() {
       });
 
     playerContainer.appendChild(youtubeContainer);
-
-    // Use Spotify API
-    var spotifyPlaylistId = 'PLAYLIST_ID';
-    var spotifyApiUrl = `https://api.spotify.com/v1/playlists/${spotifyPlaylistId}/tracks`;
-
-    // Create Spotify playlist container
-    var spotifyContainer = document.createElement('div');
-    spotifyContainer.className = 'playlist-container';
-
-    // Create Spotify Embed
-    var spotifyPlayer = document.createElement('iframe');
-    spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${spotifyPlaylistId}`;
-    spotifyPlayer.width = '300';
-    spotifyPlayer.height = '380';
-    spotifyPlayer.allowtransparency = true;
-    spotifyPlayer.allow = 'encrypted-media';
-
-    fetch(spotifyApiUrl, {
-      headers: {
-        Authorization: `Bearer ${SPOTIFY_API_KEY}`
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        var tracks = data.items;
-        var firstTrackUri = tracks[0].track.uri;
-        spotifyPlayer.src = `https://open.spotify.com/embed/track/${firstTrackUri.split(':')[2]}`;
-        spotifyContainer.appendChild(spotifyPlayer);
-      })
-      .catch(error => {
-        console.error('Error loading Spotify playlist:', error);
-      });
-
-    playerContainer.appendChild(spotifyContainer);
-  } else if (selectedGenre === 'pop') {
-    // Add information for 'pop' genre
-  } else if (selectedGenre === 'jazz') {
-    // Add information for 'jazz' genre
-  } else if (selectedGenre === 'country') {
-    // Add information for 'country' genre
-  } else if (selectedGenre === 'hiphop') {
-    // Add information for 'hiphop' genre
+    }
   }
-});
+);
